6ab06972bdd63c347e6f01a16fc0869b52b847e9
Johannes Winkler <jw.artery22@web.de>
2017-11-01 13:51:32 +0100

    -

--- Dev-Sim/Assets/Scripts/Entities/Company.cs
+++ Dev-Sim/Assets/Scripts/Entities/Company.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Enums;
 using UnityEngine;
 
 /// <summary>
 /// Class summary goes here...
 /// </summary>
-class Company : MonoBehaviour
+public class Company : MonoBehaviour
 {
     #region Fields
     #region SerializedFields
@@ -15,6 +16,17 @@ class Company : MonoBehaviour
     #endregion
 
     #region Properties
+    public string Name { get; protected set; }
+    public float Reputation { get; protected set; }
+    public float WorkingAtmosphere { get; protected set; }
+    public List<CompanyTrait> Traits { get; protected set; }
+    public float CreditWorthiness { get; protected set; }
+    public Dictionary<enField, List<Tool>> Tools { get; protected set; }
+    public Finances Finances { get; protected set; }
+    public List<Worker> Employees { get; protected set; }
+    public List<Project> Projects { get; protected set; }
+    public List<Product> Products { get; protected set; }
+
     #endregion
 
     #region Constructors
